8edbf73487130192dc44dc7530cbfd68b519b1e8
Rafkraft <Raf@Raf.local>
2013-12-18 14:23:25 +0100

    change

--- recherches/barreslide - raf/getext.js
+++ recherches/barreslide - raf/getext.js
@@ -3,13 +3,13 @@
 function go() {
 		// lancement des fonctions avec les paramètres correspondant
 		// à chaque curseur
-		draggable (1,"#cursor1", "#p1", 'triste');
-		draggable (2,"#cursor2", "#p2", 'sentimental');
-		draggable (3,"#cursor3", "#p3", 'roque');
-		draggable (4,"#cursor4", "#p4", 'régulier');
+		draggable (1,"#cursor1", "#p1", 'triste',"#slide1","#pi1");
+		draggable (2,"#cursor2", "#p2", 'sentimental',"#slide2","#pi2");
+		draggable (3,"#cursor3", "#p3", 'roque',"#slide3","#pi3");
+		draggable (4,"#cursor4", "#p4", 'régulier',"#slide4","#pi4");
 
 
-function draggable(number,cursor, p, feeling) {
+function draggable(number,cursor, p, feeling, slide, pi) {
 
 
 // fonction draggable avec jquery, su l'axe X
@@ -21,12 +21,13 @@ $( cursor ).on( "drag", function( event, ui ) {
 
 var posx = $(cursor).css("left"); //position en pixel :
 var posx = posx.replace('px', ''); //enlever le 'px'
-var posx = posx/19.2; // la position passe de 0->192 à 0->10
+var posx = posx/25.6; // la position passe de 0->192 à 0->10
 var posx = Math.floor(posx); // on enlèvre la partie décimale
 
 
 // On écrit la variable dans le paragraphe correspondant
-$(p).html(feeling + ' : '+ posx);
+$(cursor).html(posx);
+$(p).html(feeling);
 
 } );
  	
