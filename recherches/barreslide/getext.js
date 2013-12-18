b43d680b6eaa2f8c3847dd27f8e16a45168cacc6
Samuel Szoniecky <samszon@gmail.com>
2013-12-18 17:55:36 +0100

    dégradé slide

--- recherches/barreslide/getext.js
+++ recherches/barreslide/getext.js
@@ -3,38 +3,28 @@
 function go() {
 		// lancement des fonctions avec les paramètres correspondant
 		// à chaque curseur
-		draggable (1,"#cursor1", "#p1", 'triste');
-		draggable (2,"#cursor2", "#p2", 'sentimental');
-		draggable (3,"#cursor3", "#p3", 'roque');
-		draggable (4,"#cursor4", "#p4", 'régulier');
-
-
-function draggable(number,cursor, p, feeling) {
-
-
-// fonction draggable avec jquery, su l'axe X
-$( cursor ).draggable({ axis: "x" , containment: "parent" });
-
-// À chaque fois que la division est draggée
-$( cursor ).on( "drag", function( event, ui ) {
-
-
-var posx = $(cursor).css("left"); //position en pixel :
-var posx = posx.replace('px', ''); //enlever le 'px'
-var posx = posx/19.2; // la position passe de 0->192 à 0->10
-var posx = Math.floor(posx); // on enlèvre la partie décimale
-
-
-// On écrit la variable dans le paragraphe correspondant
-$(p).html(feeling + ' : '+ posx);
-
-} );
- 	
-
-
-
-
-
+		draggable (1, 'triste');
+		draggable (2, 'sentimental');
+		draggable (3, 'roque');
+		draggable (4, 'régulier');
 };
 
-};
+function draggable(number, feeling) {
+
+	// fonction draggable avec jquery, su l'axe X
+	$("#cursor"+number).draggable({ axis: "x" , containment: "parent" });
+	
+	// À chaque fois que la division est draggée
+	$("#cursor"+number).on( "drag", function( event, ui ) {
+		var posx = $("#cursor"+number).css("left"); //position en pixel :
+		posx = posx.replace('px', ''); //enlever le 'px'
+		posx = posx/19.2; // la position passe de 0->192 à 0->10
+		posx = Math.floor(posx); // on enlèvre la partie décimale
+		
+		$("#slide"+number).css("background","linear-gradient(to right, rgb(231,197,234) 50%,rgb(168,229,248) 100%)"); //position en pixel :
+		
+		// On écrit la variable dans le paragraphe correspondant
+		$("#p"+number).html(feeling + ' : '+ posx);
+		
+		} );
+	};
