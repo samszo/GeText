1f7e9b705b7023e63376a4ecb33988cb5a11a91b
Samuel Szoniecky <samszon@gmail.com>
2014-01-06 15:06:04 +0100

    ajout propositions

---
+++ application/controllers/PropositionsController.php
@@ -0,0 +1,25 @@
+<?php
+
+class PropositionsController extends Zend_Controller_Action
+{
+
+    public function indexAction()
+    {
+    	
+    }
+
+    public function romaneAction()
+    {
+    	
+    }
+    
+    public function sophieAction()
+    {
+    	
+    }
+    
+    
+}
+
+
+
