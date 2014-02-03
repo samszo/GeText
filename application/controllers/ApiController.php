461dc8feda30dffcc9279e326a955f12f944ec1e
Samuel Szoniecky <samszon@gmail.com>
2014-02-03 14:40:28 +0100

    interactions proposition Sophie

--- application/controllers/ApiController.php
+++ application/controllers/ApiController.php
@@ -5,7 +5,8 @@ class ApiController extends Zend_Controller_Action
 	
 	var $oeu = 37;
 	var $cpt = 158393;
-	var $uri = "http://generator.digitalartimag.org/services/api.php";
+	//var $uri = "http://generator.digitalartimag.org/services/api.php";
+	var $uri = "http://localhost/generateur/services/api.php";
 	
     public function indexAction()
     {
@@ -17,7 +18,7 @@ class ApiController extends Zend_Controller_Action
     	$this->view->params = $this->_request->getQuery();
     	
 	    $uri = $this->uri."?oeu=".$this->oeu."&cpt=".$this->cpt."&nb=1&frt=frg";
-		$http = new Zend_Http_Client($uri);
+	    $http = new Zend_Http_Client($uri);
 		$html = $http->request();
 		$this->view->params["texte"] = $html->getBody();
     }
@@ -27,7 +28,6 @@ class ApiController extends Zend_Controller_Action
     	if ($this->_getParam('texte', 0)){
     		$dbP = new Model_DbTable_Gt_poemes();
     		$this->view->idP = $dbP->ajouter(array("texte"=>$this->_getParam('texte'),"params"=>$this->_getParam('params')));
-    		$this->view->url = WEB_ROOT_AJAX.$this->_getParam('pro')."?id=".$this->view->idP;	
     	}else{
     		$this->view->idP = false;
     	}
